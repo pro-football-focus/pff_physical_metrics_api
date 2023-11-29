97c88e89ed42425ec082f2625784df7268d8bd8e
apschram <84464847+apschram@users.noreply.github.com>
2023-11-29 16:07:29 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 29 15:37:57 2023
+
+@author: apschram
+"""
+from setuptools import setup, find_packages
+
+setup(name='PFF Physical Metrics API',
+      version='0.1',
+      description='PFF Physical Metrics API for Python',
+      author='PFF FC',
+      author_email='fchelp@pff.com',
+      url='https://github.com/pro-football-focus/pff_physical_metrics_api/',
+      packages=find_packages(),
+      py_modules=['functions'],
+      install_requires=['pandas','requests']
+      )
