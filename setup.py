f50fca8521ab854abd834b22a751ce86ab0d3368
apschram <alexander.schram@pff.com>
2023-11-29 16:29:09 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,13 @@ Created on Wed Nov 29 15:37:57 2023
 """
 from setuptools import setup, find_packages
 
-setup(name='PFF Physical Metrics API',
-      version='0.1',
-      description='PFF Physical Metrics API for Python',
-      author='PFF FC',
-      author_email='fchelp@pff.com',
-      url='https://github.com/pro-football-focus/pff_physical_metrics_api/',
-      py_modules=['pff_physical_metrics_api'],
-      install_requires=['pandas','requests']
-      )
+setup(
+    name='pff_physical_metrics_api',
+    version='0.1',
+    description='PFF Physical Metrics API for Python',
+    author='PFF FC',
+    author_email='fchelp@pff.com',
+    url='https://github.com/pro-football-focus/pff_physical_metrics_api/',
+    packages=find_packages(),  # Use find_packages to automatically discover and include all packages
+    install_requires=['pandas', 'requests']
+)
