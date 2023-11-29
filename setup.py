4b7ded1ceef68c108b40c351050248564618cf7f
apschram <alexander.schram@pff.com>
2023-11-29 16:24:49 +0100

    Changing functions name

--- setup.py
+++ setup.py
@@ -13,7 +13,6 @@ setup(name='PFF Physical Metrics API',
       author='PFF FC',
       author_email='fchelp@pff.com',
       url='https://github.com/pro-football-focus/pff_physical_metrics_api/',
-      packages=find_packages(),
-      py_modules=['functions'],
+      py_modules=['pff_physical_metrics_api'],
       install_requires=['pandas','requests']
       )
